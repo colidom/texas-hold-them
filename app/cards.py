8b5f2ea038237a8e5fe00e5d42d09e35e95fc14e
colidom <colidom@outlook.com>
2023-05-24 19:35:51 +0100

    Añadidos ficheros del programa

---
+++ app/cards.py
@@ -0,0 +1,13 @@
+class Card:
+    def __init__(self) -> None:
+        pass
+
+
+class Deck:
+    def __init__(self) -> None:
+        pass
+
+
+class Hand:
+    def __init__(self) -> None:
+        pass
---
+++ app/roles.py
@@ -0,0 +1,8 @@
+class Dealer:
+    def __init__(self) -> None:
+        pass
+
+
+class Player:
+    def __init__(self) -> None:
+        pass
